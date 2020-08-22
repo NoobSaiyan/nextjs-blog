1929d3259473f7442ae1b422de58e971eda2570b
NoobSaiyan <ms19961998@gmail.com>
2020-08-22 13:38:45 +0530

    update test

--- pages/index.js
+++ pages/index.js
@@ -12,10 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          I am a front-end developer who loves to design UI and also animation
-          :)
-        </p>
+        <p>I just want to understand how this all is working.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
